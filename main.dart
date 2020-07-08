240d185aa4586d0d7ee78f728b3539707c707087
Hacktin <isues41@gmail.com>
2020-07-08 15:08:43 +0300

    RequestName parameter has been added from TakeTheRequest function

--- main.dart
+++ main.dart
@@ -6,17 +6,17 @@ import 'operations/abstract/IBasicCalculateOperation.dart';
 import 'operations/concerete/BasicNumberOperations.dart';
 import 'requestController.dart';
 
-num TakeTheRequest(IRequestHandler handler){
-  return handler.ValidateToRequest(10, 30, "Toplam");
+num TakeTheRequest(IRequestHandler handler,String requestName){
+  return handler.ValidateToRequest(requestName);
 }
 
 
 main(){
 
-  IBasicCalculateOperation operation=new BasicNumberOperation();
+  IBasicCalculateOperation operation=new BasicNumberOperation(10.5,20.75);
   IBasicCalculateOperationService operationService=new BasicCalculateOperationService(operation);
 
   IRequestHandler requestHandler=new BasicCalculateRequestHandler(operationService);
 
-  print(TakeTheRequest(requestHandler));
+  print(TakeTheRequest(requestHandler,"Toplam"));
 }
