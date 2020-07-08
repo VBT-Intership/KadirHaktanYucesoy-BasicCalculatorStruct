e1019fccbd5b8f68cccb7770ccdc0a258c4cf88f
Hacktin <isues41@gmail.com>
2020-07-08 15:00:14 +0300

    Inside the BasicNumberOperation class has been edited and you can take number's values from constructor method

--- operations/concerete/BasicNumberOperations.dart
+++ operations/concerete/BasicNumberOperations.dart
@@ -4,23 +4,29 @@ import '../abstract/IBasicCalculateOperation.dart';
 
 
 class BasicNumberOperation implements IBasicCalculateOperation{
-  @override
-  num add(num firstNumber, num secondNumber) {
+
+  num firstNumber;
+  num secondNumber;
+
+  BasicNumberOperation(num _firstNumber,num _secondNumber){
+    this.firstNumber=_firstNumber;
+    this.secondNumber=_secondNumber;
+  }
+
+
+  num add() {
     return firstNumber+secondNumber;
   }
 
-  @override
-  num division(num firstNumber, num secondNumber) {
+  num division() {
     return firstNumber-secondNumber;
   }
 
-  @override
-  num extract(num firstNumber, num secondNumber) {
+  num extract() {
     return firstNumber/secondNumber;
   }
 
-  @override
-  num multiplication(num firstNumber, num secondNumber) {
+  num multiplication() {
     return firstNumber*secondNumber;
   }
    
