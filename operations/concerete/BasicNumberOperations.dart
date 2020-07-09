81ee324b264d471f7eea6fe809b46fcea21b2131
Hacktin <isues41@gmail.com>
2020-07-09 08:57:59 +0300

    extract and division's operation was mixed to each other. I fixed to returning values from methods

--- operations/concerete/BasicNumberOperations.dart
+++ operations/concerete/BasicNumberOperations.dart
@@ -1,33 +1,27 @@
-
-
 import '../abstract/IBasicCalculateOperation.dart';
 
-
-class BasicNumberOperation implements IBasicCalculateOperation{
-
+class BasicNumberOperation implements IBasicCalculateOperation {
   num firstNumber;
   num secondNumber;
 
-  BasicNumberOperation(num _firstNumber,num _secondNumber){
-    this.firstNumber=_firstNumber;
-    this.secondNumber=_secondNumber;
+  BasicNumberOperation(num _firstNumber, num _secondNumber) {
+    this.firstNumber = _firstNumber;
+    this.secondNumber = _secondNumber;
   }
 
-
   num add() {
-    return firstNumber+secondNumber;
+    return firstNumber + secondNumber;
   }
 
   num division() {
-    return firstNumber-secondNumber;
+    return firstNumber / secondNumber;
   }
 
   num extract() {
-    return firstNumber/secondNumber;
+    return firstNumber - secondNumber;
   }
 
   num multiplication() {
-    return firstNumber*secondNumber;
+    return firstNumber * secondNumber;
   }
-   
 }
