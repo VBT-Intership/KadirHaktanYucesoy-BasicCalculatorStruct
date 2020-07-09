dd9731298381d065850cfae26184f86ebc31c2f7
Hacktin <isues41@gmail.com>
2020-07-09 08:49:57 +0300

    BasicCalculateRequestHandler does not extend from IRequestHandler.BasicCalculateRequestHandler implements from IRequestHandler

--- requestController.dart
+++ requestController.dart
@@ -1,49 +1,40 @@
-
-
-
 import 'business/abstract/IBasicCalculateOperationService.dart';
 
-
- class IRequestHandler{
-  num ValidateToRequest(String request){
+class IRequestHandler {
+  num ValidateToRequest(String request) {
     throw new UnimplementedError();
   }
 }
 
-class BasicCalculateRequestHandler extends IRequestHandler{
+class BasicCalculateRequestHandler implements IRequestHandler {
+  IBasicCalculateOperationService _basicCalculateservice;
+  BasicCalculateRequestHandler(IBasicCalculateOperationService service) {
+    this._basicCalculateservice = service;
+  }
 
+  num ValidateToRequest(String request) {
+    switch (request) {
+      case "Toplam":
+        {
+          return _basicCalculateservice.add();
+        }
 
-   IBasicCalculateOperationService _basicCalculateservice;
-   BasicCalculateRequestHandler(IBasicCalculateOperationService  service){
-     this._basicCalculateservice=service;
-   }
+      case "Çıkarma":
+        {
+          return _basicCalculateservice.extract();
+        }
 
+      case "Çarpma":
+        {
+          return _basicCalculateservice.multiplication();
+        }
 
-    num ValidateToRequest(String request){
-       switch(request){
-         case "Toplam":{
-            return _basicCalculateservice.add();
-         }
+      case "Bölme":
+        {
+          return _basicCalculateservice.division();
+        }
+    }
 
-         case "Çıkarma":{
-            return _basicCalculateservice.extract();
-         }
-
-         case "Çarpma":{
-            return _basicCalculateservice.multiplication();
-         }
-
-         case "Bölme":{
-           return _basicCalculateservice.division();
-         }
-            
-       }
-
-       return null;
-
-
-   }
-
-
+    return null;
+  }
 }
-
